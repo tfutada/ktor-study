82930ffb7ae10285b48bf4ea9afceb25caf7cd0c
tfutada <tafutada@gmail.com>
2024-03-19 16:07:40 +0900

    ktor client

--- src/main/kotlin/com/example/myclient3/MyClient3.kt
+++ src/main/kotlin/com/example/myclient3/MyClient3.kt
@@ -1,13 +1,20 @@
 package jp.tf.jp.tf.coroutine1
 
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.coroutines.*
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+
 
 fun main() = runBlocking {
-    repeat(50_000) { // launch a lot of coroutines
+    val client = HttpClient(CIO)
+
+    repeat(10) { // launch a lot of coroutines
         launch {
-            delay(5000L)
+//            delay(5000L)
+            val response: HttpResponse = client.get("http://localhost:8080")
             print(".")
         }
     }
 }
-
