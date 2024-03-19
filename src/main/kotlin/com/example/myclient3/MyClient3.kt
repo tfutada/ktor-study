16f6665ab19c07bd8d29b6e2dbd3f64b0ce63992
tfutada <tafutada@gmail.com>
2024-03-19 16:54:06 +0900

    ktor client

--- src/main/kotlin/com/example/myclient3/MyClient3.kt
+++ src/main/kotlin/com/example/myclient3/MyClient3.kt
@@ -8,16 +8,21 @@ import io.ktor.client.engine.cio.*
 
 
 fun main() = runBlocking {
+    val NUM_COROUTINES = 1000
+
     val client = HttpClient(CIO) {
         engine {
             requestTimeout = 20000 // Example: 20 seconds
+            endpoint.maxConnectionsPerRoute = NUM_COROUTINES
         }
     }
-    repeat(1000) { // launch a lot of coroutines
+    repeat(NUM_COROUTINES) { // launch a lot of coroutines
         launch {
 //            delay(5000L)
             val response: HttpResponse = client.get("http://localhost:8080/delay")
             print(".")
         }
     }
+
+    client.close()
 }
