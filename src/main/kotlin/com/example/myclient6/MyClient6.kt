3b4c4b00f347873f392a25ab406c28f97f47f765
tfutada <tafutada@gmail.com>
2024-03-20 10:11:18 +0900

    ktor client

--- src/main/kotlin/com/example/myclient6/MyClient6.kt
+++ src/main/kotlin/com/example/myclient6/MyClient6.kt
@@ -2,15 +2,29 @@ package com.example.myclient6
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.async
 
 fun main() = runBlocking {
-    val client = HttpClient(CIO)
-    val requestCount = 3 // Number of times to repeat the request
-    val url = "http://localhost:8080/"
+    val TIMEOUT = 60_000L
+
+    val client = HttpClient(CIO) {
+//        engine {
+//            endpoint.maxConnectionsPerRoute = NUM_COROUTINES
+//        }
+        install(HttpTimeout) {
+            connectTimeoutMillis = TIMEOUT // Increase connection timeout to 30 seconds
+            // Other timeout settings:
+//            requestTimeoutMillis = TIMEOUT
+//            socketTimeoutMillis = TIMEOUT
+        }
+    }
+
+    val requestCount = 1000 // Number of times to repeat the request
+    val url = "http://localhost:8080/delay"
 
     val requests = List(requestCount) {
         async { client.get(url).bodyAsText() }
