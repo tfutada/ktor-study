8283dce9c1167cc8d57fee86b376b4ee388706dc
tfutada <tafutada@gmail.com>
2024-03-20 20:01:14 +0900

    file upload

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,9 +1,12 @@
 package com.example.plugins
 
+import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.delay
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
@@ -13,6 +16,32 @@ fun Application.configureRouting() {
         get("/delay") {
             delay(3000L)
             call.respondText("Hello World!999111")
+        }
+
+        var fileDescription = ""
+        var fileName = ""
+
+        post("/upload") {
+            val multipartData = call.receiveMultipart()
+
+            multipartData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        fileDescription = part.value
+                    }
+
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName as String
+                        val fileBytes = part.streamProvider().readBytes()
+                        File("uploads/$fileName").writeBytes(fileBytes)
+                    }
+
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            call.respondText("$fileDescription is uploaded to 'uploads/$fileName'")
         }
     }
 }
