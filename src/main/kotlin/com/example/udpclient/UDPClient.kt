50d502dce05bdd6d9cd92edd76b2040c1765f545
tfutada <tafutada@gmail.com>
2024-04-05 22:22:44 +0900

    udp client

--- src/main/kotlin/com/example/udpclient/UDPClient.kt
+++ src/main/kotlin/com/example/udpclient/UDPClient.kt
@@ -1,15 +1,21 @@
+package com.example.udpclient
+
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 
 fun main() {
-    val serverAddress = InetAddress.getByName("::1")
-    val serverPort = 5106
+    try {
+        val serverAddress = InetAddress.getByName("::1")
+        val serverPort = 5106
 
-    val data = "Hello, UDP Server!".toByteArray()
-    val clientSocket = DatagramSocket()
-    val packet = DatagramPacket(data, data.size, serverAddress, serverPort)
-    clientSocket.send(packet)
-    clientSocket.close()
+        val data = "Hello, UDP Server!".toByteArray()
+        DatagramSocket().use { clientSocket ->
+            val packet = DatagramPacket(data, data.size, serverAddress, serverPort)
+            clientSocket.send(packet)
+            // Socket is automatically closed
+        }
+    } catch (e: Exception) {
+        e.printStackTrace() // Or handle the exception as appropriate
+    }
 }
-
