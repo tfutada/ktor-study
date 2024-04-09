82d83e60bacefc156bd45d72372498af337bfa8f
tfutada <tafutada@gmail.com>
2024-04-09 18:38:36 +0900

    POST timeout

--- src/main/kotlin/com/example/posts/Post1.kt
+++ src/main/kotlin/com/example/posts/Post1.kt
@@ -1,8 +1,8 @@
 package com.example.posts
 
-
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -18,19 +18,31 @@ suspend fun main() {
         install(ContentNegotiation) {
             json()
         }
-    }
-
-    val response: HttpResponse = client.post("https://jsonplaceholder.typicode.com/posts") {
-        headers {
-            append(HttpHeaders.Authorization, "abc123")
-            append(HttpHeaders.UserAgent, "ktor client")
+        install(HttpTimeout) {
+            connectTimeoutMillis = 10_000
+            requestTimeoutMillis = 10_000
         }
-        contentType(ContentType.Application.Json)
-        setBody(Post("foo", "bar", 1))
     }
 
-    println(response.bodyAsText())
+    try {
+        val response: HttpResponse = client.post("https://jsonplaceholder.typicode.com/posts") {
+            headers {
+                append(HttpHeaders.Authorization, "abc123")
+                append(HttpHeaders.UserAgent, "ktor client")
+            }
+            contentType(ContentType.Application.Json)
+            setBody(Post("foo", "bar", 1))
+        }
+
+        if (response.status.value in 200..299) {
+            println("Success: ${response.bodyAsText()}")
+        } else {
+            println("Failed with status code: ${response.status.value}")
+        }
+    } catch (e: Exception) {
+        println("An error occurred: ${e.message}")
+        e.printStackTrace() // This will dump the stack trace to standard error stream
+    } finally {
+        client.close()
+    }
 }
-
-
-
