02c94e2934ca9e7f46847fdd75c6c40893eb653c
tfutada <tafutada@gmail.com>
2024-05-14 11:28:22 +0700

    fix error handling

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,7 +5,11 @@ import io.ktor.server.application.*
 import kotlin.time.Duration.Companion.seconds
 import io.ktor.server.plugins.ratelimit.*
 
+// you can run this main function to start the server
+// or you can run the server by running the gradle task `run`
+// NB. in gradle case, the main function is not called.
 fun main(args: Array<String>) {
+    println("starting from the main function directly. you won't see this message if you run the server using gradle task `run`. ")
     io.ktor.server.netty.EngineMain.main(args)
 }
 
